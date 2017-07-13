f2f2a2ae918d7a48fc3bd8e8771ae8a8d101e036
Sylvain Julé <sylvain.jule@free.fr>
2017-07-13 18:10:36 +0200

    Color option

--- fields/imageradio/imageradio.php
+++ fields/imageradio/imageradio.php
@@ -7,7 +7,7 @@ class ImageradioField extends InputField {
             'styles.css'
         )
     );
-    
+
     public $columns = 2;
     protected $cache;
 
@@ -56,90 +56,132 @@ class ImageradioField extends InputField {
     public function item($value, $options) {
 
         $input = $this->input($value);
-        
-        // Get label and filename of the input
+
+        // Get the input's label
         $text = $options['label'];
-        $image = $options['image'];
+
+        // Get display options
         $display = $this->display();
-        
-        
-        /* Get image URL
+
+
+        /* Defaults
         ----------------------------*/
-        
-        // If a query is made about images, set parent directory as page URI or 'page' option 
-        if ($this->query() && $this->query()['fetch'] == 'images') {
-            
-            $query = $this->query();
-            
-            // If there is a 'page' options -> Get the page URI
-            if (array_key_exists('page', $query)) {
-                $uri = $this->getPage($query['page'])->uri();
-            } 
-            // If there is no 'page' option -> Get the current page URI
-            else {
-                $uri = $this->page()->uri();
-            }
-            
-            $imageurl = kirby()->urls->index() . '/' . $uri . '/' . $image;
-        }
-        // Otherwise, image is to be found in the main assets/images folder
-        else {
-            $imageurl = kirby()->urls()->assets() .'/images/'. $image;
-        }
-        
-        
-        /* Mobile option
-        ----------------------------*/
-        
-        // default : false
+
+        $ratio = false;
         $mobile = false;
-        
-        // Get mobile option if specified
-        if ($display && array_key_exists('mobile', $display)) {
-            $mobile = $display['mobile'];
-        }
-        
-        $mobileClass = $mobile ? '' : ' mobile-disabled';
-        
-        
-        /* If there is no ratio
+        $position = 'center center';
+
+
+        /* Display options
         ----------------------------*/
-        
-        // Go with an img tag if there's no ratio specified
-        if (!$display || !array_key_exists('ratio', $display)) {
-            $imageDiv = '<img src="'. $imageurl .'">';
-            $imageDiv = '<div class="radio-illustration'. $mobileClass .'">'. $imageDiv .'</div>';
-        }
-        
-        
-        /* If there is a ratio
-        ----------------------------*/
-        
-        if ($display && array_key_exists('ratio', $display)) {
-            
-            // Get and convert ratio (3/2 -> 66.6%)
-            $ratio = $display['ratio'];
-            $convertedRatio = 1;
-            
-            if (preg_match('/(\d+)(?:\s*)([\/])(?:\s*)(\d+)/', $ratio, $matches) !== false){
-                $convertedRatio = $matches[3] / $matches[1];
+
+        if ($display) {
+
+            /* Mobile
+            ----------------------------*/
+
+            // Get mobile option if specified
+            if (array_key_exists('mobile', $display)) {
+                $mobile = $display['mobile'];
             }
-            
-            $convertedRatio = $convertedRatio * 100;
-            $convertedRatio = round($convertedRatio, 3, PHP_ROUND_HALF_DOWN);
-            $ratio = $convertedRatio;
-            
-            // Get position if specified | default : centered
-            $position = array_key_exists('position', $display) ? $display['position'] : 'center center';
-            
-            $imageDiv = '<div class="radio-illustration as-background'. $mobileClass .'" style="background-image: url('. $imageurl .'); background-position: '. $position .'; padding-top: '. $ratio .'%;"></div>';
-            
+
+            /* Ratio
+            ----------------------------*/
+
+            if (array_key_exists('ratio', $display)) {
+
+                // Get and convert ratio (3/2 -> 66.666%)
+                $ratio = $display['ratio'];
+                $convertedRatio = 1;
+
+                // Do the math
+                if (preg_match('/(\d+)(?:\s*)([\/])(?:\s*)(\d+)/', $ratio, $matches) !== false){
+                    $convertedRatio = $matches[3] / $matches[1];
+                }
+
+                $convertedRatio = $convertedRatio * 100;
+                $convertedRatio = round($convertedRatio, 3, PHP_ROUND_HALF_DOWN);
+
+                $ratio = $convertedRatio;
+            }
+
+
+            /* Position
+            ----------------------------*/
+
+            if (array_key_exists('position', $display)) {
+                $position = $display['position']; 
+            }
+
         }
-        
-        
+
+        $mobileClass = $mobile ? '' : ' mobile-disabled';
+
+
+        /* Color
+        ----------------------------*/
+
+        // If the 'color' option is specified
+        if (array_key_exists('color', $options)) {
+
+            $color = $options['color'];
+            $ratio = ($ratio) ? $ratio : 25;
+
+            $imageDiv = '<div class="radio-illustration as-background'. $mobileClass .'" style=" padding-top: '. $ratio .'%; background-color: '. $color .';"></div>';
+        }
+
+
+        /* Image
+        ----------------------------*/
+
+        // If the 'image' option is specified
+        elseif (array_key_exists('image', $options)) {
+
+            $image = $options['image'];
+
+            /* Get image URL
+            ----------------------------*/
+
+            // If a query is made about images, set parent directory as page URI or 'page' option 
+            if ($this->query() && $this->query()['fetch'] == 'images') {
+
+                $query = $this->query();
+
+                // If there is a 'page' options -> Get the page URI
+                if (array_key_exists('page', $query)) {
+                    $uri = $this->getPage($query['page'])->uri();
+                } 
+                // If there is no 'page' option -> Get the current page URI
+                else {
+                    $uri = $this->page()->uri();
+                }
+
+                $imageurl = kirby()->urls->index() . '/' . $uri . '/' . $image;
+            }
+            // Otherwise, image is to be found in the main assets/images folder
+            else {
+                $imageurl = kirby()->urls()->assets() .'/images/'. $image;
+            }
+
+            /* Build the DIV
+            ----------------------------*/
+
+            // If no ratio
+            if (!$ratio) {
+                $imageDiv = '<img src="'. $imageurl .'">';
+                $imageDiv = '<div class="radio-illustration'. $mobileClass .'">'. $imageDiv .'</div>';
+            }
+            // If ratio is specified
+            else {
+                $imageDiv = '<div class="radio-illustration as-background'. $mobileClass .'" style="background-image: url('. $imageurl .'); background-position: '. $position .'; padding-top: '. $ratio .'%;"></div>';
+            }
+
+        }
+
+
         /* Build the input
         ----------------------------*/
-        
+
         $label = new Brick('label');
         $label->addClass('input');
         $label->addClass('input-with-radio');
@@ -155,11 +197,11 @@ class ImageradioField extends InputField {
         return $label;
 
     }
-    
+
     public function getPage($uri) {
-        
+
         if(str::startsWith($uri, '../')) {
-            
+
             if($currentPage = $this->page) {
                 $path = $uri;
                 while(str::startsWith($path, '../')) {
@@ -184,7 +226,7 @@ class ImageradioField extends InputField {
         }
         return $page;
     }
-    
+
     public function content() {
 
         $html = '<ul class="input-list field-grid cf">';
@@ -208,8 +250,8 @@ class ImageradioField extends InputField {
         }
 
         foreach($this->options() as $key => $value) {
-            
-            
+
+
             $html .= '<li class="input-list-item field-grid-item' . $width . '">';
             $html .= $this->item($key, $value);
             $html .= '</li>';
