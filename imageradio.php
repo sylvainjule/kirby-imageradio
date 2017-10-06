85307bea56030b886856a978b05766981b020ff6
Sylvain Julé <sylvain.jule@free.fr>
2017-10-06 09:58:51 +0200

    Shorter Image object creation

--- imageradio.php
+++ imageradio.php
@@ -55,7 +55,6 @@ class ImageradioField extends InputField {
 
     public function item($value, $options) {
 
-    	$kirby = kirby();
         $input = $this->input($value);
 
         // Get the input's label
@@ -161,7 +160,7 @@ class ImageradioField extends InputField {
             }
             // Otherwise, image is to be found in the main assets/images folder
             else {
-            	$image = new Media ($kirby->roots()->assets() .'/images/'. $image, $kirby->urls()->assets() .'/images/'. $image);
+            	$image = new Asset('assets/images/' . $image);
                 $imageurl = $image->resize(400)->url();
             }
 
