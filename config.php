5d8e016d5d70e95915d8b5ea872faae9b752c7ea
Sylvain Julé <sylvain.jule@free.fr>
2018-11-12 09:27:29 +0100

    Clean config.php

--- config.php
+++ config.php
@@ -4,13 +4,4 @@ Kirby::plugin('sylvainjule/imageradio', [
 	'fields' => array(
 		'imageradio' => require_once __DIR__ . '/lib/fields/imageradio.php',
 	),
-    'methods' => array(
-        'getImageRadioOptions' => function () {
-            return Options::factory(
-                $this->options(),
-                $this->props,
-                $this->model()
-            );
-        },
-    )
 ]);
