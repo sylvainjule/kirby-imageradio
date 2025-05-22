6fa3acd37968c4bc9d772c2713997f668c604a39
Nico Hoffmann <me@nhoffmann.com>
2025-05-22 08:59:15 +0200

    fix: Remove `Kirby\Blueprint\Factory` use

--- lib/options/imageradio-option.php
+++ lib/options/imageradio-option.php
@@ -2,10 +2,6 @@
 
 namespace SylvainJule;
 
-use Kirby\Blueprint\Factory;
-use Kirby\Blueprint\NodeIcon;
-use Kirby\Blueprint\NodeText;
-use Kirby\Blueprint\NodeString;
 use Kirby\Cms\ModelWithContent;
 
 
@@ -13,34 +9,19 @@ class ImageRadioOption extends \Kirby\Option\Option {
     public function __construct(
         public string|int|float|null $value,
         public bool $disabled = false,
-        public NodeIcon|null $icon = null,
-        public NodeText|null $info = null,
-        public NodeText|null $text = null,
-        public NodeString|null $image = null,
+        public string|null $image = null,
+         public string|null $icon = null,
+		public string|array|null $info = null,
+		string|array|null $text = null
     ) {
-        $this->text ??= new NodeText(['en' => $this->value]);
+		$this->text = $text ?? ['en' => $this->value];
     }
-    public static function factory(string|int|float|null|array $props): static
-    {
 
-        $props = ImageRadioFactory::apply($props, [
-            'icon'  => NodeIcon::class,
-            'info'  => NodeText::class,
-            'text'  => NodeText::class,
-            'image' => NodeString::class,
-        ]);
-
-        return new static(...$props);
-    }
     public function render(ModelWithContent $model): array
     {
         return [
-            'disabled' => $this->disabled,
-            'icon'     => $this->icon?->render($model),
-            'info'     => $this->info?->render($model),
-            'text'     => $this->text?->render($model),
-            'image'    => $this->image?->render($model),
-            'value'    => $this->value
+            ...parent::render($model),
+            'image' => $this->image
         ];
     }
 }
